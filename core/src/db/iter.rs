1168980da89fe2fb9fd070824b837f5c99e3c9ef
naoki <2629936804@qq.com>
2024-12-31 16:59:08 +0800

    update: fix error

--- core/src/db/iter.rs
+++ core/src/db/iter.rs
@@ -18,28 +18,24 @@ impl<'a, K: Serialize + DeserializeOwned + Clone, V: Serialize + DeserializeOwne
     type Item = (Cow<'a, K>, Cow<'a, V>);
 
     fn next(&mut self) -> Option<Self::Item> {
-        let (key, value) = self
-            .db_iter
-            .next()?
-            .map_err(|e| {
-                tracing::debug!("RocksDB Iter iterator error: {e}");
-            })
-            .ok()?;
+        if let Some(Ok((key, value))) = self.db_iter.next() {
+            // Deserialize the key and value.
+            let key = bincode::deserialize(&key[self.prefix.len()..])
+                .map_err(|e| {
+                    tracing::debug!("RocksDB Iter deserialize(key) error: {e}");
+                })
+                .ok()?;
 
-        // Deserialize the key and value.
-        let key = bincode::deserialize(&key[self.prefix.len()..])
-            .map_err(|e| {
-                tracing::debug!("RocksDB Iter deserialize(key) error: {e}");
-            })
-            .ok()?;
+            let value = bincode::deserialize(&value)
+                .map_err(|e| {
+                    tracing::debug!("RocksDB Iter deserialize(value) error: {e}");
+                })
+                .ok()?;
 
-        let value = bincode::deserialize(&value)
-            .map_err(|e| {
-                tracing::debug!("RocksDB Iter deserialize(value) error: {e}");
-            })
-            .ok()?;
-
-        Some((Cow::Owned(key), Cow::Owned(value)))
+            Some((Cow::Owned(key), Cow::Owned(value)))
+        } else {
+            None
+        }
     }
 }
 
@@ -60,22 +56,18 @@ impl<'a, K: 'a + Clone + Serialize + DeserializeOwned> Iterator for Keys<'a, K> 
     type Item = Cow<'a, K>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let (key, _) = self
-            .db_iter
-            .next()?
-            .map_err(|e| {
-                tracing::debug!("RocksDB Keys iterator error: {e}");
-            })
-            .ok()?;
+        if let Some(Ok((key, _))) = self.db_iter.next() {
+            // Deserialize the key.
+            let key = bincode::deserialize(&key[self.prefix.len()..])
+                .map_err(|e| {
+                    tracing::debug!("RocksDB Keys deserialize(key) error: {e}");
+                })
+                .ok()?;
 
-        // Deserialize the key.
-        let key = bincode::deserialize(&key[self.prefix.len()..])
-            .map_err(|e| {
-                tracing::debug!("RocksDB Keys deserialize(key) error: {e}");
-            })
-            .ok()?;
-
-        Some(Cow::Owned(key))
+            Some(Cow::Owned(key))
+        } else {
+            None
+        }
     }
 }
 
@@ -95,21 +87,17 @@ impl<'a, V: 'a + Clone + Serialize + DeserializeOwned> Iterator for Values<'a, V
     type Item = Cow<'a, V>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let (_, value) = self
-            .db_iter
-            .next()?
-            .map_err(|e| {
-                tracing::debug!("RocksDB Values iterator error: {e}");
-            })
-            .ok()?;
+        if let Some(Ok((_, value))) = self.db_iter.next() {
+            // Deserialize the value.
+            let value = bincode::deserialize(&value)
+                .map_err(|e| {
+                    tracing::debug!("RocksDB Values deserialize(value) error: {e}");
+                })
+                .ok()?;
 
-        // Deserialize the value.
-        let value = bincode::deserialize(&value)
-            .map_err(|e| {
-                tracing::debug!("RocksDB Values deserialize(value) error: {e}");
-            })
-            .ok()?;
-
-        Some(Cow::Owned(value))
+            Some(Cow::Owned(value))
+        } else {
+            None
+        }
     }
 }
